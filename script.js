af3955c7fe210867a723354ea636afaac66bf2aa
luisxgarcia <luis15fer08@gmail.com>
2024-09-11 14:54:32 -0600

    v1.0

--- script.js
+++ script.js
@@ -27,34 +27,13 @@ function handleScroll(event) {
     }, 1000);
 }
 
-function handleTouchStart(event) {
-    touchStartY = event.touches[0].clientY;
-}
 
-function handleTouchMove(event) {
-    touchEndY = event.touches[0].clientY;
-}
-
-function handleTouchEnd() {
-    if (isThrottled) return;
-    isThrottled = true;
-
-    if (touchStartY > touchEndY + 50) {
-        scrollToSection(currentSection + 1);
-    } else if (touchStartY < touchEndY - 50) {
-        scrollToSection(currentSection - 1);
-    }
-
-    setTimeout(() => {
-        isThrottled = false;
-    }, 1000);
-}
 
 let lastScrollTop = 0;
 window.addEventListener('scroll', function () {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     if (scrollTop > lastScrollTop) {
-        nav.style.top = '-60px'; // Oculta el menú al desplazarse hacia abajo
+        nav.style.top = '-50px'; // Oculta el menú al desplazarse hacia abajo
     } else {
         nav.style.top = '0'; // Muestra el menú al desplazarse hacia arriba
     }
@@ -62,9 +41,6 @@ window.addEventListener('scroll', function () {
 });
 
 window.addEventListener('wheel', handleScroll);
-window.addEventListener('touchstart', handleTouchStart);
-window.addEventListener('touchmove', handleTouchMove);
-window.addEventListener('touchend', handleTouchEnd);
 
 // Manejo informacion JSON
 
@@ -77,26 +53,34 @@ fetch(jsondata)
         const area = data["area"];
         let description = data["description"];
         let aboutme = data["aboutme"];
+        let school = data["school"];
+        let jobs = data["jobs"];
         let characteristics = data["characteristics"];
         let cursos = data["cursos"];
         let extra = data["extra"];
         const technologies = data["technologies"];
         const projects = data["projects"];
+        const contact = data["contact"];
 
         description = description.replace(/\n/g, '<br>');
         aboutme = aboutme.replace(/\n/g, '<br>');
-        characteristics = characteristics.replace(/\n/g, '<br>');
+        characteristics = characteristics.replace(/<br>/g, '<li>');
         extra = extra.replace(/\n/g, '<br>');
 
         document.getElementById("name-short").innerHTML = nameShort;
         document.getElementById("area").innerHTML = area;
         document.getElementById("description").innerHTML = description;
-        document.getElementById("about-me").innerHTML = aboutme;
-        document.getElementById("characteristics").innerHTML = "<h3>Características</h3>" + characteristics + "<br>" + "<h3>Cursos</h3>";
+        document.getElementById("characteristics").innerHTML = "<h3>Características</h3>" + characteristics;
         document.getElementById("extra").innerHTML = "<h3>Algo mas...</h3>" + extra;
 
+        for (let i = 0; i < school.length; i++) {
+            document.getElementById("school").innerHTML += "<i class='bi bi-backpack-fill'></i> <b>" + school[i] + "</b><br><br>";
+        }
+        for (let i = 0; i < jobs.length; i++) {
+            document.getElementById("jobs").innerHTML += "<b> <i class='bi bi-person-gear'></i> " + jobs[i]+ "</b><br><br>";
+        }
         for (let i = 0; i < cursos.length; i++) {
-            document.getElementById("characteristics").innerHTML += "<b>" + cursos[i].name + "</b> " + " <br> " + cursos[i].description + "<br> <br>";
+            document.getElementById("courses").innerHTML += "<i class='bi bi-file-text-fill'></i> <b>" + cursos[i].name + "</b> " + " <br>" + cursos[i].description + "<br> <br>";
         }
 
         for (let i = 0; i < technologies.length; i++) {
@@ -121,8 +105,14 @@ fetch(jsondata)
             projectHTML += `
                             </ul>
                         </div>
+                        <br>
                     </div>
+                    <div class="buttons">
+                            <button><i class="bi bi-youtube"> <a href="${projects[i].url}" target="_blank">Demo</a></i></button>
+                            <button><i class="bi bi-github"> <a href="${projects[i].github}" target="_blank">Código</a></i></button>
+                        </div>
                 </div>
+                
             `;
             document.getElementById("projects").innerHTML += projectHTML;
         }
@@ -141,9 +131,63 @@ fetch(jsondata)
             }
         `;
         document.head.appendChild(style);
-        
     })
     .catch(error => {
         console.error('Error al cargar el archivo JSON:', error);
     });
 
+    //Manejo de popup
+    const close = document.getElementById("close");
+    const contact = jsondata["contact"];
+    fetch(jsondata)
+    .then(response => response.json())
+    .then(data => {
+        const contact = data["contact"];
+        const trigger = document.getElementById("trigger");
+        const overlay = document.getElementById("overlay");
+        
+        const text = document.getElementById("text");
+        const github = document.getElementById("github");
+        const linkedin = document.getElementById("linkedin");
+        const email = document.getElementById("mail");
+
+        github.addEventListener("click", () => {
+            for(let i = 0; i < contact.length; i++){
+                if(contact[i].type == "github-personal"){
+                    text.innerHTML += "<a href='" + contact[i].value + "'>" + "<h3>GitHub Personal</h3>"+ "</a><br>";
+                }
+                else if(contact[i].type == "github-edu"){
+                    text.innerHTML += "<a href='" + contact[i].value + "'>" + "<h3>GitHub Escolar</h3>"+ "</a>";
+                }
+            }
+            overlay.style.display = (overlay.style.display === 'none' || overlay.style.display === '') ? 'block' : 'none';
+            trigger.style.display = 'none';
+        })
+        email.addEventListener("click", () => {
+            for(let i = 0; i < contact.length; i++){
+                if(contact[i].type == "email"){
+                    text.innerHTML += "<h3>Correo</h3><br>"+ "<p>" + contact[i].value + "</p>"; 
+                }
+            }
+            overlay.style.display = (overlay.style.display === 'none' || overlay.style.display === '') ? 'block' : 'none';
+            trigger.style.display = 'none';
+        })
+        linkedin.addEventListener("click", () => {
+            for(let i = 0; i < contact.length; i++){
+                if(contact[i].type == "linkedin"){
+                    text.innerHTML += "<a href='" + contact[i].value + "'>" + "<br><br><h3>LinkedIn</h3>"+ "</a>";
+                }
+            }
+            overlay.style.display = (overlay.style.display === 'none' || overlay.style.display === '') ? 'block' : 'none';
+            trigger.style.display = 'none';
+        })
+        
+    })
+    close.addEventListener("click", () => {
+        overlay.style.display = 'none';
+        trigger.style.display = 'block';
+        text.innerHTML = "";
+    }
+    )
+    
+    
